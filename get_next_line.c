03a5436d7fdbdaa150d9b17b565f14a5815bc6fb
Gabriel Teixeira araujo <gateixei@c2r9s7.42lisboa.com>
2022-07-28 20:56:13 +0100

    half

--- get_next_line.c
+++ get_next_line.c
@@ -12,53 +12,49 @@
 
 #include "get_next_line.h"
 
-char *fix_buffer(char *buf, int i)
-{
-
-}
-
 int ft_endline(char *str)
 {
     int i;
 
     i = 0;
-    while (str[i] != '\n' && (i + 1) <= BUFFER_SIZE)
+    while (str[i] != '\n' && (i) < BUFFER_SIZE)
         str[i++];
     return (i);
 }
 
-char *get_next_line(int fd, static char *str)
+char *get_next_line(int fd)
 {
     char *buf;
+    static char *rtn;
     int i;
+    int c;
+    int r;
 
-    buf = (char*)malloc(BUFFER_SIZE * sizeof(char));
-    read(fd, buf, BUFFER_SIZE);
-    i = ft_endline(buf);
-    if (i < (int) BUFFER_SIZE)
-    {
-        //buf = ;
-
-       // printf("%s", buf);
-        
-        return (buf);
+    c = 0;
+    i = BUFFER_SIZE;
+    while (i >= BUFFER_SIZE)
+    {         
+        buf = (char*)malloc(BUFFER_SIZE * sizeof(char));
+        read(fd, buf, BUFFER_SIZE);
+        i = ft_endline(buf);
+        c++;
     }
-    else {
-	str = 
-        get_next_line(fd);
-    }
+    printf("%i", c);
+    r = (BUFFER_SIZE * c) - (BUFFER_SIZE - i);
+    rtn = (char*)malloc(r * sizeof(char));
+    read(fd, rtn, r);
+    //free = buf;
+    return (rtn);
 }
+
 
 int main(void)
 {
-    static char *str;
+    char *str;
     int fd;
-    int i;
     
     fd = open("texto.txt", O_RDONLY);
     str = get_next_line(fd);
-    //str = get_next_line(fd);
-    //str = get_next_line(fd);
     printf("%s", str);
     return 0;
 }
