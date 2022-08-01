f5df6e6d7a90c6410fcf6451dd0fb52f49354576
pro-gabrielteixeira <pro.gabrielteixeira@gmail.com>
2022-08-01 17:27:27 +0100

    Half way

--- get_next_line.c
+++ get_next_line.c
@@ -12,41 +12,76 @@
 
 #include "get_next_line.h"
 
-int ft_endline(char *str)
+int	ft_strlen(const char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new_str;
+	int		i;
+	int		n;
+	int		c;
+	int		p;
+
+	i = 0;
+	c = 0;
+	p = 0;
+	if (!s1 || !s2)
+		return (0);
+	n = ft_strlen(s1) + ft_strlen(s2);
+	new_str = (char *) malloc((n + 1) * (sizeof(char)));
+	if (!new_str)
+		return (0);
+	while (s1[c] != '\0')
+		new_str[p++] = s1[c++];
+	while (s2[i] != '\0')
+		new_str[p++] = s2[i++];
+	new_str[p] = '\0';
+    free(s1);
+	return (new_str);
+}
+
+
+int next_line_char(char *buf)
 {
     int i;
 
     i = 0;
-    while (str[i] != '\n' && (i) < BUFFER_SIZE)
-        str[i++];
-    return (i);
+    while (i <= (BUFFER_SIZE + 1) && buf[i] != '\0')
+    {
+        if (buf[i] == '\n')
+            return (i);
+        i++;
+    }
+    return (0);
 }
+
 
 char *get_next_line(int fd)
 {
-    char *buf;
-    static char *rtn;
+    char    *line;
+    char    *buf;
     int i;
-    int c;
-    int r;
 
-    c = 0;
-    i = BUFFER_SIZE;
-    while (i >= BUFFER_SIZE)
-    {         
-        buf = (char*)malloc(BUFFER_SIZE * sizeof(char));
-        read(fd, buf, BUFFER_SIZE);
-        i = ft_endline(buf);
-        c++;
-    }
-    printf("%i", c);
-    r = (BUFFER_SIZE * c) - (BUFFER_SIZE - i);
-    rtn = (char*)malloc(r * sizeof(char));
-    read(fd, rtn, r);
-    //free = buf;
-    return (rtn);
+    buf = (char*)malloc(BUFFER_SIZE * sizeof(char));
+    read(fd, buf, BUFFER_SIZE);
+    line = (char*)malloc(sizeof(char));
+    line[0] = '\0';
+    i = next_line_char(buf);
+    if (!i) //beware from EOF exception, infinite loop
+            line = get_next_line(fd);
+    else if (buf[i] != '\n' || BUFFER_SIZE > (i + 1)) //add check when buffer is bigger than line
+        printf("ELSE\n");
+    line = ft_strjoin(buf, line);
+    return (line);
 }
-
 
 int main(void)
 {
@@ -55,6 +90,10 @@ int main(void)
     
     fd = open("texto.txt", O_RDONLY);
     str = get_next_line(fd);
-    printf("%s", str);
+    printf("palavra - %s", str);
+    str = get_next_line(fd);
+    printf("palavra - %s", str);
+    str = get_next_line(fd);
+    printf("palavra - %s", str);
     return 0;
 }
